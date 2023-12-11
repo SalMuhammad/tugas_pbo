00ccfb928312219752d9a03fbc2605f851c77018
mohamad Faisal <ngomongaja17@gmail.com>
2023-12-12 01:03:57 +0700

    menambahkan form untuk menamambahkan kitab meskipun belum berfungsi

--- interaktif.js
+++ interaktif.js
@@ -6,29 +6,41 @@ import {
 
 
 
-
 // ----------------------------------------------------------
 // ==================   HALAMAN USER   =======================
 // ----------------------------------------------------------
 
-$('#yg-di-tampilkan').addEventListener('click', e => {
-  if(e.target.className.split(' ').includes('fixed')) { //munculkan halaman rincian pesanan saat tombol selanjutnya di klik
-    $('#rincian-pesanan').classList.remove('hidden') 
-  } else if(e.target.className.split(' ').includes('fa-arrow-left')){
-    $('#rincian-pesanan').classList.add('hidden')
-  }
-})
+if($('#yg-di-tampilkan')){
+  $('#yg-di-tampilkan').addEventListener('click', e => {
+    if(e.target.className.split(' ').includes('fixed')) { //munculkan halaman rincian pesanan saat tombol selanjutnya di klik
+      $('#rincian-pesanan').classList.remove('hidden') 
+    } else if(e.target.className.split(' ').includes('fa-arrow-left')){
+      $('#rincian-pesanan').classList.add('hidden')
+    }
+  })
+}
 
-// $('.nav').addEventListener('click', e => {
-
-// })
-
+// bagian footer
 Array.from($all('footer nav i')).forEach(i => {
   i.addEventListener('click', e => {
     $all('footer nav i').forEach(i => i.classList.remove("text-red-600"))
     i.classList.add('text-red-600')
   })
 })
+
+/*----------------------------------------------------------------------
+========================   HALAMAN KELOLA KITAB   ======================
+----------------------------------------------------------------------*/
+
+$('#tambah-data').addEventListener('click', function() {
+  $('#layouting').classList.remove('hidden')
+})
+$('#close').addEventListener('click', function() {
+  $('#layouting').classList.add('hidden')
+})
+
+
+
 
 
 
